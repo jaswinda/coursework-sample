b775f06b662da1728f39fb222f829d3cb02c3fd0
jaswinda <singhjassi2624@gmail.com>
2023-04-27 07:39:22 +0545

    fix: combo box example

--- src/coursework2/views/AddCreditCard.java
+++ src/coursework2/views/AddCreditCard.java
@@ -3,6 +3,7 @@ package coursework2.views;
 import coursework2.BankCard;
 import coursework2.CreditCard;
 import coursework2.components.MyCustomButton;
+import coursework2.components.MyCustomComboBox;
 import coursework2.components.MyCustomLabel;
 import coursework2.components.MyCustomTextField;
 
@@ -12,11 +13,12 @@ import java.util.ArrayList;
 
 public class AddCreditCard extends JFrame {
     MyCustomTextField t1, t2, t3, t4, t5, t6, t7, t8;
+    MyCustomComboBox c1, c2, c3;
     MyCustomLabel l1, l2, l3, l4, l5, l6, l7, l8, l9;
     MyCustomButton b1, b2, b3;
     public AddCreditCard(ArrayList<BankCard> bankCards){
         super("Add Credit Card");
-        setSize(500, 600);
+        setSize(500, 800);
         setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
@@ -46,10 +48,25 @@ public class AddCreditCard extends JFrame {
         t7 = new MyCustomTextField("");
         t8 = new MyCustomTextField("");
 
+        //years
+        c1 = new MyCustomComboBox(new String[]{
+                "select year", "2016", "2017", "2018", "2019",
+                "2020", "2021", "2022", "2023", "2024", "2025"});
+        //months
+        c2 = new MyCustomComboBox(new String[]{
+                "select month",
+                "01", "02", "03", "04", "05", "06", "07", "08", "09", "10","11","12"});
+        //days
+        c3 = new MyCustomComboBox(new String[]{"select day",
+                "01", "02", "03", "04", "05", "06", "07", "08", "09", "10","11","12","13","14","15","16","17","18","19","20",
+        		"21","22","23","24","25","26","27","28","29","30","31"});
+
 
         b1 = new MyCustomButton("Add");
         b2 = new MyCustomButton("Clear");
         b3 = new MyCustomButton("Display");
+
+
 
         b1.addActionListener(e -> {
             String cardId = t1.getText();
@@ -59,15 +76,17 @@ public class AddCreditCard extends JFrame {
             String balanceAmount = t5.getText();
             String cvcNumber = t6.getText();
             String interestRate = t7.getText();
-            String expirationDate = t8.getText();
 
-            if (cardId.equals("") || clientName.equals("") || issueBank.equals("") || bankAccount.equals("") || balanceAmount.equals("") || cvcNumber.equals("") || interestRate.equals("") || expirationDate.equals("")) {
+            if (cardId.equals("") || clientName.equals("") || issueBank.equals("") || bankAccount.equals("") || balanceAmount.equals("") || cvcNumber.equals("") || interestRate.equals("")
+                    || c1.getSelectedItem().equals("select year") || c2.getSelectedItem().equals("select month") || c3.getSelectedItem().equals("select day")
+            ) {
                 JOptionPane.showMessageDialog(null, "Please fill all the fields");
             } else {
                 int card = Integer.parseInt(cardId);
                 double balance = Double.parseDouble(balanceAmount);
                 int cvc = Integer.parseInt(cvcNumber);
                 double interest = Double.parseDouble(interestRate);
+                String expirationDate = c1.getSelectedItem() + "-" + c2.getSelectedItem() + "-" + c3.getSelectedItem();
                 CreditCard creditCard = new CreditCard(card, clientName, issueBank, bankAccount, balance, cvc, interest, expirationDate);
                 bankCards.add(creditCard);
                 JOptionPane.showMessageDialog(null, "Credit Card added successfully");
@@ -172,15 +191,26 @@ public class AddCreditCard extends JFrame {
         gbc.gridx = 1;
         gbc.gridy = 8;
         gbc.gridwidth = 1;
-        panel.add(t8, gbc);
+        panel.add(c1, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 9;
+        gbc.gridwidth = 1;
+        panel.add(c2, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 10;
+        gbc.gridwidth = 1;
+        panel.add(c3, gbc);
+
 
         gbc.gridx = 0;
-        gbc.gridy = 9;
+        gbc.gridy = 11;
         gbc.gridwidth = 1;
         panel.add(b2, gbc);
 
         gbc.gridx = 1;
-        gbc.gridy = 9;
+        gbc.gridy = 11;
         gbc.gridwidth = 1;
         panel.add(b1, gbc);
 
@@ -196,5 +226,8 @@ public class AddCreditCard extends JFrame {
         t6.setText("");
         t7.setText("");
         t8.setText("");
+        c1.setSelectedIndex(0);
+        c2.setSelectedIndex(0);
+        c3.setSelectedIndex(0);
     }
 }
